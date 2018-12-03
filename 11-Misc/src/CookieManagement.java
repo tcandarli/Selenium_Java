bfa79a6ab6421857ef20d8ecd73cd40d6adb29c0
tcandarli <tcandarli@gmail.com>
2018-12-03 17:43:45 +0100

    CookieManagement

---
+++ 11-Misc/src/CookieManagement.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CookieManagement {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "..\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("httpw://www.cnn.com/");
+
+		Set<Cookie> ck = driver.manage().getCookies();
+
+		for (Cookie cookie : ck) {
+			System.out.println(cookie.getName() + "|" + cookie.getValue());
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().deleteCookieNamed("_cb_svref");
+
+	}
+
+}
