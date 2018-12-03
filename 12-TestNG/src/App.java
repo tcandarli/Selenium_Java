3d0a9f8498350eca8e0f5270a5cd79726a52a9c2
tcandarli <tcandarli@gmail.com>
2018-12-03 18:35:24 +0100

    TestNG

---
+++ 12-TestNG/src/App.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.Test;
+
+public class App {
+
+	@Test
+	public void LoginTest() {
+		// selenium code
+		System.out.println("Login");
+	}
+
+	@Test
+	public void PasswordChange() {
+		System.out.println("Changing password");
+	}
+
+	@Test
+	public void LogoutTest() {
+		System.out.println("Logout");
+	}
+
+}
