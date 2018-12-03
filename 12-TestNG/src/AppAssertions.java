1ebfdbc0796a849b0b602eff61584252d3932384
tcandarli <tcandarli@gmail.com>
2018-12-03 21:58:02 +0100

    Assertions

---
+++ 12-TestNG/src/AppAssertions.java
@@ -0,0 +1,37 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AppAssertions {
+	
+	SoftAssert sa = new SoftAssert();
+
+	@Test
+	public void RegisterTest() {
+//		String expectedTitle = "yahoo";
+//		String actualTitle = "yahoo1";
+//
+//		System.out.println("A");
+//		
+//		Assert.assertEquals(actualTitle, expectedTitle);
+//		
+//		System.out.println("B");
+//		
+//		Assert.assertTrue(2>11, "Error");
+		
+		
+		///////////////////////////////
+		
+		String expectedTitle = "yahoo";
+		String actualTitle = "yahoo1";
+		
+		System.out.println("A");
+		sa.assertEquals(actualTitle, expectedTitle);
+		
+		System.out.println("B");
+		sa.assertEquals("a", "c");
+		sa.assertAll();
+		
+	}
+
+}
