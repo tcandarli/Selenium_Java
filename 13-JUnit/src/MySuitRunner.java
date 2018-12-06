520302d228993c6fc996bbce50c05676c2b8d4a6
tcandarli <tcandarli@gmail.com>
2018-12-06 17:36:17 +0100

    Maven Java

--- 13-JUnit/src/MySuitRunner.java
+++ 13-JUnit/src/MySuitRunner.java
@@ -2,11 +2,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
 @RunWith(Suite.class)
 
 // Add test classes into {} to run all of them
-@SuiteClasses({TestApplication.class, Assertions.class, Parameterization.class})
+@SuiteClasses({ TestApplication.class, Assertions.class, Parameterization.class })
 public class MySuitRunner {
 
 }
