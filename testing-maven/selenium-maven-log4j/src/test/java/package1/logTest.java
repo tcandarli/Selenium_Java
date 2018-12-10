b77c0f4192095fe8b7ca7543a71fb9f11586e705
tcandarli <tcandarli@gmail.com>
2018-12-10 17:14:52 +0100

    selenium maven log4j

--- testing-maven/selenium-maven-log4j/src/test/java/package1/logTest.java
+++ testing-maven/selenium-maven-log4j/src/test/java/package1/logTest.java
@@ -8,7 +8,16 @@ public class logTest {
 	public static void main(String[] args) {
 
 		Logger log = LogManager.getLogger(logTest.class.getName());
+		log.debug("I am logging"); // When each selenium action is performed like click(),
+		// sendKeys.getText()
+		log.info("Object is present"); // When operation is succesfully completed
+		// ex: After loading page
+		log.debug("starting");
 
+		if (0 > 2) {
+			log.info("Object is present");
+		} else {
+			log.error("Object is not present");
+		}
 	}
-
 }
