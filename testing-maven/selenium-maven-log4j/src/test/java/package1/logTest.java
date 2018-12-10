2fc1ecfe609c2c32c0c8bc12294a5cdbd09942e4
tcandarli <tcandarli@gmail.com>
2018-12-10 17:07:15 +0100

    selenium maven log4j

---
+++ testing-maven/selenium-maven-log4j/src/test/java/package1/logTest.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class logTest {
+
+	public static void main(String[] args) {
+
+		Logger log = LogManager.getLogger(logTest.class.getName());
+
+	}
+
+}
