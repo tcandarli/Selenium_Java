22ba5ea1cb3d75f8eb046784c6ef405d5cfdecb8
tcandarli <tcandarli@gmail.com>
2018-12-11 18:33:41 +0100

    selenium maven grid

--- testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest.java
+++ testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest.java
@@ -9,20 +9,16 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class gridTest {
-	
+
 	@Test
-	public void login () throws MalformedURLException {
-		
+	public void login() throws MalformedURLException {
+
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
 		caps.setPlatform(Platform.ANY);
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.0-206:4444/wd/hub"),caps);
-		
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.206:4444/wd/hub"), caps);
+		driver.get("https://www.amazon.com");
+		//driver.quit();
+
 	}
-	
-	
-	
-	
-	
-	
 
 }
--- testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest2.java
+++ testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest2.java
@@ -1,5 +1,24 @@
 package com.grid;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
 public class gridTest2 {
+
+	@Test
+	public void login() throws MalformedURLException {
+
+		DesiredCapabilities caps = DesiredCapabilities.firefox();
+		caps.setPlatform(Platform.ANY);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.206:4444/wd/hub"), caps);
+		driver.get("https://www.cnn.com");
+		//driver.quit();
+
+	}
 
 }
