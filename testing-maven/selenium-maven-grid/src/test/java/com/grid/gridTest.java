06db74f003e746dfb753cad41161ab0807c55746
tcandarli <tcandarli@gmail.com>
2018-12-11 18:20:08 +0100

    selenium maven grid

--- testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest.java
+++ testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest.java
@@ -1,10 +1,28 @@
 package com.grid;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 public class gridTest {
 	
-	DesiredCapabilities caps = DesiredCapabilities.chrome();
+	@Test
+	public void login () throws MalformedURLException {
+		
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setPlatform(Platform.ANY);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.0-206:4444/wd/hub"),caps);
+		
+	}
+	
+	
+	
+	
+	
 	
 
 }
