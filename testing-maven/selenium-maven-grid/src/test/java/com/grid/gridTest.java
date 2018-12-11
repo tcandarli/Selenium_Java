83b1ca53b6da835dadd384ff49d58bd3d3ddf620
tcandarli <tcandarli@gmail.com>
2018-12-11 18:09:54 +0100

    selenium maven grid

---
+++ testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest.java
@@ -0,0 +1,10 @@
+package com.grid;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class gridTest {
+	
+	DesiredCapabilities caps = DesiredCapabilities.chrome();
+	
+
+}
---
+++ testing-maven/selenium-maven-grid/src/test/java/com/grid/gridTest2.java
@@ -0,0 +1,5 @@
+package com.grid;
+
+public class gridTest2 {
+
+}
