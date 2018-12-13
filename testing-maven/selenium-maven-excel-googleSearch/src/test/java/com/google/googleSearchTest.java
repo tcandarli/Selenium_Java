e8d86ed35bdfde4e3fd9184ecd8136c66d260d96
tcandarli <tcandarli@gmail.com>
2018-12-13 21:21:59 +0100

    selenium maven excel

--- testing-maven/selenium-maven-excel-googleSearch/src/test/java/com/google/googleSearchTest.java
+++ testing-maven/selenium-maven-excel-googleSearch/src/test/java/com/google/googleSearchTest.java
@@ -2,27 +2,48 @@ package com.google;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class googleSearchTest {
-	
+
 	WebDriver driver = null;
-	
+	Xls_Reader data = new Xls_Reader("C:\\Users\\PC\\Documents\\github\\Selenium\\testing-maven\\selenium-maven-excel-googleSearch\\src\\test\\resources\\testData.xlsx");
+
 	@BeforeTest
 	public void setUp() {
-		
+
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
-		
-		
-		
+
+	}
+
+	@AfterTest
+	public void closeUp() {
+		driver.quit();
+	}
+
+	@Test
+	public void searchResults() {
+
+		driver.findElement(By.name("q")).sendKeys(item);
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		String result = driver.findElement(By.id("resultatStats")).getText();
+		result = result.replace("About ", "");
+		result = result.replace(" results", "");
+		driver.navigate().back();
+
 	}
 
 }
