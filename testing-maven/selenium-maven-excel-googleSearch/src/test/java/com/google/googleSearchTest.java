3e2aadc4632c513c6ed094a9e202c1ff2b81d28c
tcandarli <tcandarli@gmail.com>
2018-12-13 21:06:32 +0100

    selenium maven excel googleSearch

---
+++ testing-maven/selenium-maven-excel-googleSearch/src/test/java/com/google/googleSearchTest.java
@@ -0,0 +1,28 @@
+package com.google;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleSearchTest {
+	
+	WebDriver driver = null;
+	
+	@BeforeTest
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		
+		
+		
+	}
+
+}
