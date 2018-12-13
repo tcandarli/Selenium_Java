25e3b81f95a866f19fa3ff30caa567cc902d5237
tcandarli <tcandarli@gmail.com>
2018-12-13 21:34:50 +0100

    selenium maven excel googleSearch

--- testing-maven/selenium-maven-excel-googleSearch/src/test/java/com/google/googleSearchTest.java
+++ testing-maven/selenium-maven-excel-googleSearch/src/test/java/com/google/googleSearchTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -36,13 +35,25 @@ public class googleSearchTest {
 
 	@Test
 	public void searchResults() {
+		
+		int rcount = data.getRowCount("data");
+		
+		for (int i = 2; i<rcount; i++) {
+			
+			String item = data.getCellData("data", "Item", i);
+			driver.findElement(By.name("q")).sendKeys(item);
+			driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+			String result = driver.findElement(By.id("resultatStats")).getText();
+			result = result.replace("About ", "");
+			result = result.replace(" results", "");
+			data.setCellData("data", "SearchResults", i, result);
+			
+			driver.navigate().back();
+			
+			
+		}
 
-		driver.findElement(By.name("q")).sendKeys(item);
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-		String result = driver.findElement(By.id("resultatStats")).getText();
-		result = result.replace("About ", "");
-		result = result.replace(" results", "");
-		driver.navigate().back();
+		
 
 	}
 
