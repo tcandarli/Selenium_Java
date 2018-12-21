1b877366741393be6393ca81a71237ddb30d35f5
tcandarli <tcandarli@gmail.com>
2018-12-21 14:36:32 +0100

    selenium maven pom

---
+++ testing-maven/selenium-maven-pom/src/test/java/pages/WebOrderLoginPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+
+public class WebOrderLoginPage {
+	
+	public WebElement username;
+	public WebElement password;
+	public WebElement loginButton;
+	
+	
+
+}
---
+++ testing-maven/selenium-maven-pom/src/test/java/pomdesign/WebOrderLoginTest.java
@@ -0,0 +1,46 @@
+package pomdesign;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebOrderLoginTest {
+
+	WebDriver driver;
+	
+	
+	@BeforeTest
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+	}
+	
+	
+	@AfterTest
+	public void closeUp() {
+		driver.quit();
+	}
+	
+	@Test
+	public void login() {
+		driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+		driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+		driver.findElement(By.id("ctl00_MainContent_login_button")).click();
+		
+	}
+	
+	
+	
+	
+	
+}
