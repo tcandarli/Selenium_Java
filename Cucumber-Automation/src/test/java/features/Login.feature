2954ac608d2ec053d131be72aaa1697ec86de319
tcandarli <tcandarli@gmail.com>
2018-12-28 21:05:48 +0100

    step definition

--- Cucumber-Automation/src/test/java/features/Login.feature
+++ Cucumber-Automation/src/test/java/features/Login.feature
@@ -2,6 +2,12 @@ Feature: Application login
 
 Scenario: Home page default login 
 	Given User is on NetBanking landing page 
-	When User login into application with username and password 
+	When User login into application with "John" and password "321"
 	Then Home page is populated 
-	And Cards are displayed
+	And Cards displayed are "true"
+	
+Scenario: Home page default login 
+	Given User is on NetBanking landing page 
+	When User login into application with "Mike" and password "123"
+	Then Home page is populated 
+	And Cards displayed are "false"
--- Cucumber-Automation/src/test/java/stepDefinitions/stepDefiniton.java
+++ Cucumber-Automation/src/test/java/stepDefinitions/stepDefiniton.java
@@ -1,4 +1,4 @@
-package stepDefinitons;
+package stepDefinitions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
@@ -11,35 +11,31 @@ import org.junit.runner.RunWith;
 public class stepDefiniton {
 
 	@Given("^User is on NetBanking landing page$")
-    public void user_is_on_netbanking_landing_page() throws Throwable {
-        
-        // code to navigate to login url
-        System.out.println("Navigated to login url");
-       
-    }
+	public void user_is_on_NetBanking_landing_page() throws Throwable {
+		System.out.println("Navigated to url");
 
-	@When("^User login into application with username and password$")
-	public void user_login_into_application_with_username_and_password() throws Throwable {
+	}
 
-		// code to login
-		System.out.println("Logged in success");
-		
+	@When("^User login into application with \"([^\"]*)\" and password \"([^\"]*)\"$")
+	public void user_login_into_application_with_something_and_password_something(String strArg1, String strArg2)
+			throws Throwable {
+
+		System.out.println(strArg1);
+		System.out.println(strArg2);
+
 	}
 
 	@Then("^Home page is populated$")
 	public void home_page_is_populated() throws Throwable {
+		System.out.println("Home page is populated");
 
-		// home page validation
-		System.out.println("Validated home page");
-		
 	}
 
-	@And("^Cards are displayed$")
-	public void cards_are_displayed() throws Throwable {
+	@Then("^Cards displayed are \"([^\"]*)\"$")
+	public void cards_displayed_are(String arg1) throws Throwable {
 
-		// validating the cards
-		System.out.println("Validated the cards");
-		
+		System.out.println(arg1);
+
 	}
 
 }
