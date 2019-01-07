3ef8d4fbc6c32f4c9325dca200739b7a04eda965
tcandarli <tcandarli@gmail.com>
2019-01-07 21:05:51 +0100

    RESTApi JsonPath

--- cucumber-testng-automation/src/test/java/tests/API_Post_Requests.java
+++ cucumber-testng-automation/src/test/java/tests/API_Post_Requests.java
@@ -1,6 +1,18 @@
 package tests;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.login.Configuration;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import utilities.ConfigurationReader;
 
 public class API_Post_Requests {
 	
@@ -18,6 +30,28 @@ public class API_Post_Requests {
 		Then status code should be 201
 		And response body should match request body
 	*/
+	
+	@Test
+	public void postNewRegion () {
+		
+		String url = ConfigurationReader.getProperty("hrapp.baseresturl") + "/regions/";
+		
+		// String requestJson = "{\"region_id\" : 6,\"region_name\" : \"new region\"}";
+		
+		Map requestMap = new HashMap<>();
+		requestMap.put("region_id", 55);
+		requestMap.put("region_name", "new region");
+		
+		Response response =  given().accept(ContentType.JSON)
+		.and().contentType(ContentType.JSON)
+		.and().body(requestMap)
+		.when().post(url);
+		
+		System.out.println(response.statusLine());
+		response.prettyPrint();
+		
+		
+	}
 	
 	
 
